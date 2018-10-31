7a113ed7e5e938a1c65db27b05ad019aac8a5fcb
Your Name <you@example.com>
2018-10-31 14:31:56 -0600

    Añadi Login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { Http }  from '@angular/http';
 
 /**
  * Generated class for the LoginPage page.
@@ -9,15 +10,18 @@ import { HomePage } from '../home/home';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
+@IonicPage ()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
   home = HomePage;
+  usuario  = '';
+  password = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+    
   }
 
   ionViewDidLoad() {
@@ -26,8 +30,22 @@ export class LoginPage {
 
   clickHome(){
   	console.log("clickHome");
+    console.log(this.usuario);
+    console.log(this.password);
+
+    this.http.get('/home/')
+    .subscribe(data => {
+      console.log(data.text());
+      
+    }, error => {
+      console.log("error");
+      
+    });
+    console.log();
+    
     //this.navCtrl.push(this.home);
-    this.navCtrl.setRoot(this.home);
+    
+    //this.navCtrl.setRoot(this.home);
   }
 
 
